962dffd923630e66c1f4c6c0257051f929b79416
byelde <byelacademico@gmail.com>
2025-02-13 15:29:56 -0300

    ✨ feat: Implementing basic Routes and generic Providers

--- webview-ui/src/app/App.tsx
+++ webview-ui/src/app/App.tsx
@@ -1,9 +1,13 @@
+import { Routes } from "./routes"
+import { DataProvider } from "./shared/contexts"
+
+
 function App() {
   
   return (
-    <>
-      <h1>HELLO WORLD!</h1>
-    </>
+    <DataProvider>
+      <Routes/>
+    </DataProvider>
   )
 }
 
