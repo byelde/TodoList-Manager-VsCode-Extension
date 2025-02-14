de5f13ab56fa2599d6f1ac73b0a9935b646a8197
byelde <byelacademico@gmail.com>
2025-02-14 13:07:28 -0300

    :sparkles: feat: Implementing the Todo List
    Integrating Todo components and Implementing the Todo List

--- webview-ui/src/app/pages/home/Home.tsx
+++ webview-ui/src/app/pages/home/Home.tsx
@@ -1,9 +1,29 @@
+import { TodoInput } from "../../shared/components/TodoInput";
+import { TodoListElement } from "../../shared/components/TodoListElement";
+import { useTodoContext } from "../../shared/contexts";
+
 function Home() {
   
+  const{ todoList } = useTodoContext();
+
   return (
-    <>
-      <h1>HOME PAGE!</h1>
-    </>
+    <div>
+      <h1>Home Page</h1>
+      <p>Hello World!</p>
+
+      <TodoInput/>
+      <ul>
+        {todoList.map(
+          ( todoListItem )=>(
+            <TodoListElement
+              key={todoListItem.id} 
+              {...todoListItem}
+            />
+          )
+        )}
+      </ul>
+
+    </div>
   )
 }
 
