5d3fee56c94a44abd00a6f9daad65593ca461f83
byelde <byelacademico@gmail.com>
2025-02-16 16:33:47 -0300

    :sparkles: feat: Implementing Sidebar
    Implementing Todo List Sidebar and cleaning up the extension.ts code.

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,24 @@
-import { commands, ExtensionContext } from "vscode";
+import { commands, ExtensionContext, window } from "vscode";
+
 import { TodoPanel } from "./panels/TodoPanel";
+import { SidebarProvider } from "./providers/SideBarProvider";
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
+  
+  // Register the sidebar webview
+  context.subscriptions.push(
+    window.registerWebviewViewProvider(
+      "react-todo-sideBar-view", // This should match the id from the views contribution in the package.json.
+      new SidebarProvider(context.extensionUri)
+    )
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId (first param of registerCommand method) parameter must match the command field in package.json
-
+  // Register the command to activate the webview panel todolist mode
 	context.subscriptions.push(
-    // The code you place here will be executed every time your command is executed
-    commands.registerCommand('react-todo.ToDo', () => {
-      // Display a message box to the user
-      TodoPanel.render(context.extensionUri);
-    })
+    commands.registerCommand(
+      'react-todo.ToDo', // This should match the id from the commands contribution in the package.json.
+      () => {TodoPanel.render(context.extensionUri);} // Renders the current webview panel
+    )
   );
 }
