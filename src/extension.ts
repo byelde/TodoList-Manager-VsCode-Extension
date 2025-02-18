4c871744b61ae91a129d61f7a37cf023d8b2369e
byelde <byelacademico@gmail.com>
2025-02-18 15:39:49 -0300

    ✨ feat: Creating Todo List`s Context Command Adds a editor context command to register the current selected text as a todo item, also saving its value, and location.

--- src/extension.ts
+++ src/extension.ts
@@ -6,19 +6,44 @@ import { SidebarProvider } from "./providers/SideBarProvider";
 // This method is called when your extension is activated
 export function activate(context: ExtensionContext) {
   
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
   // Register the sidebar webview
   context.subscriptions.push(
     window.registerWebviewViewProvider(
       "react-todo-sideBar-view", // This should match the id from the views contribution in the package.json.
-      new SidebarProvider(context.extensionUri)
+      sidebarProvider
     )
   );
 
   // Register the command to activate the webview panel todolist mode
-	context.subscriptions.push(
+ context.subscriptions.push(
     commands.registerCommand(
       'react-todo.ToDo', // This should match the id from the commands contribution in the package.json.
       () => {TodoPanel.render(context.extensionUri);} // Renders the current webview panel
     )
   );
+
+ context.subscriptions.push( commands.registerCommand(
+    'react-todo.addTodoSelection', // This should match the id from the commands contribution in the package.json.
+    () => {
+      const activeTextEditor = window.activeTextEditor;
+
+      if (!activeTextEditor) {
+        window.showErrorMessage("No active text editor");
+        return;
+      }
+
+      const selection = activeTextEditor.selection;
+      const text = activeTextEditor.document.getText(selection);
+      const path = activeTextEditor.document.uri.path;
+      const line = selection.active.line;
+
+      // Send a message to React component TodoInput in webview-ui/src/app/shared/components/TodoInput.tsx
+      sidebarProvider._view?.webview.postMessage({
+        command: "addTodoSelection",
+        value: { text, path, line },
+      });
+    }
+  ));
+
 }
