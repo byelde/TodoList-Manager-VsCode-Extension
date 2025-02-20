73a215b9ceb34846cae6236ecec666794a87478e
byelde <byelacademico@gmail.com>
2025-02-20 16:00:55 -0300

    💡 docs: Commenting better TodoItem and TodoLocation Adding comments that better describe the types TodoItem and TodoLocation

--- webview-ui/src/app/shared/types/TodoItem.ts
+++ webview-ui/src/app/shared/types/TodoItem.ts
@@ -1,5 +1,12 @@
 import { TodoLocation } from "./TodoLocation";
 
+/**
+ * Represents a single todo item with its associated properties.
+ * @property {string} id - The unique identifier for the todo item.
+ * @property {string} text - The text content of the todo item.
+ * @property {boolean} isChecked - Indicates whether the todo item has been completed.
+ * @property {TodoLocation} location - The location associated with the todo item.
+ */
 export type TodoItem = {
   id: string;
   text: string;
--- webview-ui/src/app/shared/types/TodoLocation.ts
+++ webview-ui/src/app/shared/types/TodoLocation.ts
@@ -1,3 +1,8 @@
+/**
+ * Represents the location of a todo item within a file.
+ * @property {string | undefined} path - The file path where the todo item is located.
+ * @property {number | undefined} line - The line number where the todo item is located.
+ */
 export type TodoLocation = {
   path: string | undefined,
   line: number | undefined,
